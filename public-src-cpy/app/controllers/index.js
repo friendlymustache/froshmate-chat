9ddc5ae53b3ec637118a9c2b77098baf6d6f7253
Siddharth Murching <smurching@caltech.edu>
2015-07-22 01:45:48 -0700

    Add copy of public-src

---
+++ public-src-cpy/app/controllers/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	actions : {
+		login_success : function(user) {
+
+		},
+
+		login_failure : function(reason) {
+			
+		}
+	}
+});
---
+++ public-src-cpy/public-src/app/models/user.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  fb_user_id : DS.attr('number'),
+  email : DS.attr('string'),
+  access_token : DS.attr('string')  
+});
