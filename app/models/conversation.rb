2bbcaea80cab6853b78d60139b5724385e6b7378
Siddharth Murching <smurching@caltech.edu>
2015-07-24 15:51:45 -0700

    Switch to postgres

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,7 +3,6 @@ class Conversation < ActiveRecord::Base
 	belongs_to :student, :class_name => "User"
 	belongs_to :mentor, :class_name => "User" 
 	def self.get_conversations_for_user(user_id)
-		puts "Finding all conversations where the student or mentor id is #{user_id}"
 		return Conversation.where("student_id = ? OR mentor_id = ?", user_id, user_id)
 	end
 
@@ -12,7 +11,6 @@ class Conversation < ActiveRecord::Base
 		if user
 			result = Conversation.where("id = ? AND (student_id = ? OR mentor_id = ?)",
 			 conversation_id.to_i, user.id, user.id).first
-			puts result
 			return result
 		end
 	end
