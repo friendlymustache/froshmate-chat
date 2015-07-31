4ee609edb9772a4b29b27d92308869a183cb642f
Lahsiv <v.talasani@gmail.com>
2015-07-31 00:53:08 -0400

    Auto install node

---
+++ install_node.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install
+cd frontend
+sudo npm install
+sudo bower install
+cd bower_components
+cd semantic-ui
+npm install
+gulp build
