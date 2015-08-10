689e3169ecea9f5584dc3c05971a555caa08485e
Siddharth Murching <smurching@caltech.edu>
2015-08-10 16:48:26 -0700

    Modify production.rb to use environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
     # Allow CORS from froshmate.com
   config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'chat.froshmate.com', 'http://54.148.34.184', '54.148.34.184'
+        origins ENV['PROD_APP_URL'], "http://#{ENV['PROD_IP_ADDRESS']}", ENV['PROD_IP_ADDRESS']
         resource '*',
           :headers => :any,
           :methods => [:get, :post, :delete, :put, :options, :head],
